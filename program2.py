4e125952d315c88d26b616842f3441eead530089
pushpa626 <153419507+pushpa626@users.noreply.github.com>
2024-09-27 17:12:03 +0530

    python program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,9 @@
-def decode_message( s: str, p: str) -> bool:
+def decode_message(s: str, p: str) -> bool:
+    s_ptr, p_ptr = 0, 0
 
-# write your code here
-  
-        return False
+    while p_ptr < len(p):
+        if s_ptr < len(s) and p[p_ptr] == s[s_ptr]:
+            s_ptr += 1
+            p_ptr += 1
+        else:
+            return False
